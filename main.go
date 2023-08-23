61245963dd5b4f0324afd85b215311e571d34f8c
dedbin <black13dog@yandex.ru>
2023-08-23 13:19:53 +0300

    add arr and slc

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	Printing()
+	arrays_and_slices()
+}
+
+func arrays_and_slices() {
+	var arr = [5]int{1, 2, 3, 4, 5}
+	var slice = []int{1, 2, 3, 4, 5}
+
+	fmt.Println("Arrays and Slices")
+	fmt.Printf("Arrays in go are static arr = %v\n", arr)
+	fmt.Printf("Slices in go are dynamic arr = %v\n", slice)
+}
+
+func Printing() {
 	age := 16
 	name := "gosha"
 	str2 := "smth"
@@ -18,5 +34,5 @@ func main() {
 
 	//Printf (formated output, no \n)
 	fmt.Printf("My name is %v i am %v  years old", name, age)
-	fmt.Printf("smth = %q", str2)
+	fmt.Printf("smth = %q \n", str2)
 }
